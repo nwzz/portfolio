f539ca438fac086cc0ead5b41f391db7f9765a60
nwzz <shahnewaz@mail.com>
2022-03-03 01:46:26 +0600

    update

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,11 @@
 //import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: 'Shahnewaz',
-    title: "MERN Stack Developer",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://i.ibb.co/G5VjgBt/shahnewaz.jpg',
-    resumePdf: 'https://drive.google.com/file/d/121zfSOJ9mlsNn6LgdioMENijWMen0vxe/view'
-}
+  name: "Shahnewaz",
+  title: "MERN Stack Developer",
+  desciption:
+    "I'm a Web Developer by Profession & Hobby. I'm a Web Developer by Profession & Hobby.I Design, Develop, and Deploy Web Applications. From concept, navigation, layout and programming to UX and SEO as a MERN Stack Web Developer.",
+  image: "https://i.ibb.co/G5VjgBt/shahnewaz.jpg",
+  resumePdf:
+    "https://drive.google.com/file/d/14k1-cxgHkSeo_b3xliewE0le1Y40DCRI/view",
+};
