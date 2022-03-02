dd6f4164787866d222545fbc5165f11a6e84c9f9
nwzz <shahnewaz@mail.com>
2022-03-03 01:48:46 +0600

    update

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ export const headerData = {
   name: "Shahnewaz",
   title: "MERN Stack Developer",
   desciption:
-    "I'm a Web Developer by Profession & Hobby. I'm a Web Developer by Profession & Hobby.I Design, Develop, and Deploy Web Applications. From concept, navigation, layout and programming to UX and SEO as a MERN Stack Web Developer.",
+    "I'm a Web Developer by Profession & Hobby. I Design, Develop, and Deploy Web Applications. From concept, navigation, layout and programming to UX and SEO as a MERN Stack Web Developer.",
   image: "https://i.ibb.co/G5VjgBt/shahnewaz.jpg",
   resumePdf:
     "https://drive.google.com/file/d/14k1-cxgHkSeo_b3xliewE0le1Y40DCRI/view",
