4db905b44ef7effad1c9733682cc2d6309cb79dc
nwzz <shahnewaz@mail.com>
2022-03-16 00:20:18 +0600

    finalize

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,88 +1,96 @@
-import one from '../assets/svg/projects/one.svg'
-import two from '../assets/svg/projects/two.svg'
-import three from '../assets/svg/projects/three.svg'
-import four from '../assets/svg/projects/four.svg'
-import five from '../assets/svg/projects/five.svg'
-import six from '../assets/svg/projects/six.svg'
-import seven from '../assets/svg/projects/seven.svg'
-import eight from '../assets/svg/projects/eight.svg'
-
+import one from "../assets/svg/projects/one.svg";
+import two from "../assets/svg/projects/two.svg";
+import three from "../assets/svg/projects/three.svg";
+import four from "../assets/svg/projects/four.svg";
+import five from "../assets/svg/projects/five.svg";
+import six from "../assets/svg/projects/six.svg";
+import seven from "../assets/svg/projects/seven.svg";
+import eight from "../assets/svg/projects/eight.svg";
 
 export const projectsData = [
-    {
-        id: 1,
-        projectName: 'e-furniture-shop',
-        projectDesc: 'This project aims to build a furniture selling e-commerce website all type of functionalities implemented Users make and update profiles, add to cart, place orders with shipping information includes. ',
-        tags: ['React',' TailwindCSS','Mongoose', 'Node.js'],
-        code: '',
-        demo: 'https://e-furniture-shop.netlify.app/',
-        image: one
-    },
-    {
-        id: 2,
-        projectName: 'Nwzz-TourPacker',
-        projectDesc: 'This is a Tour Booking and travel agency related project.',
-        tags: ['React',' Bootstrap 4','Mongoose', 'Node.js'],
-        code: '',
-        demo: 'https://nwzz-tourpacker.web.app/',
-        image: two
-    },
-    {
-        id: 3,
-        projectName: 'Shoes-Gallery',
-        projectDesc: 'A MERN Stack E-Commerce Project',
-        tags: ['React',' TailwindCSS','Mongoose','Express.js', 'Node.js'],
-        code: '',
-        demo: 'https://shoes-gallery.netlify.app/',
-        image: three
-    },
-    {
-        id: 4,
-        projectName: 'Android Patient Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        code: '  ',
-        demo: '  ',
-        image: four
-    },
-    {
-        id: 5,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A Simple E-commerce application',
-        tags: ['React Native', 'Firebase'],
-        code: '  ',
-        demo: '  ',
-        image: five
-    },
-    {
-        id: 6,
-        projectName: 'Uber Lite',
-        projectDesc: 'Uber clone',
-        tags: ['Flutter'],
-        code: '  ',
-        demo: '  ',
-        image: six
-    },
-    {
-        id: 7,
-        projectName: 'Stock Market App',
-        projectDesc: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        code: '  ',
-        demo: '  ',
-        image: seven
-    },
-    {
-        id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: '  ',
-        demo: '  ',
-        image: eight
-    },
-]
-
+  {
+    id: 1,
+    projectName: "e-furniture-shop",
+    projectDesc:
+      "This project aims to build a furniture selling e-commerce website all type of functionalities implemented Users make and update profiles, add to cart, place orders with shipping information includes. ",
+    tags: ["React", " TailwindCSS", "Mongoose", "Node.js"],
+    code: "",
+    demo: "https://e-furniture-shop.netlify.app/",
+    image: one,
+  },
+  {
+    id: 2,
+    projectName: "Nwzz-TourPacker",
+    projectDesc: "This is a Tour Booking and travel agency related project.",
+    tags: ["React", " Bootstrap 4", "Mongoose", "Node.js"],
+    code: "",
+    demo: "https://nwzz-tourpacker.web.app/",
+    image: two,
+  },
+  {
+    id: 3,
+    projectName: "Shoes-Gallery",
+    projectDesc: "A MERN Stack UI E-Commerce Project",
+    tags: ["React", " TailwindCSS", "Mongoose", "Express.js", "Node.js"],
+    code: "",
+    demo: "https://shoes-gallery.netlify.app/",
+    image: three,
+  },
+  {
+    id: 4,
+    projectName: "Baby Shop",
+    projectDesc:
+      "This project involves the development of an Web application for viewing and managing multiple baby items. This is a E-Commerce Website Admin can add, edit and delete products. ",
+    tags: [
+      "React",
+      " Bootstrap 4",
+      "Mongoose",
+      "Node.js",
+      "Firebase",
+      "MongoDB",
+    ],
+    code: "  ",
+    demo: "https://nwzz-baby-shop.netlify.app/",
+    image: four,
+  },
+  {
+    id: 5,
+    projectName: "E-Commerce App",
+    projectDesc: "A Simple E-commerce application",
+    tags: ["React Native", "Firebase"],
+    code: "  ",
+    demo: "  ",
+    image: five,
+  },
+  {
+    id: 6,
+    projectName: "Uber Lite",
+    projectDesc: "Uber clone",
+    tags: ["Flutter"],
+    code: "  ",
+    demo: "  ",
+    image: six,
+  },
+  {
+    id: 7,
+    projectName: "Stock Market App",
+    projectDesc: "A simple stock market API app",
+    tags: ["React", "Redux", "Bootstrap"],
+    code: "  ",
+    demo: "  ",
+    image: seven,
+  },
+  {
+    id: 8,
+    projectName: "Car Pooling System",
+    projectDesc:
+      "The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution",
+    tags: ["Flutter", "React"],
+    code: "  ",
+    demo: "  ",
+    image: eight,
+  },
+];
 
 // Do not remove any fields.
 // Leave it blank instead as shown below
